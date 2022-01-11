f577fc3d30a14efbd6513735babb010be648cf5f
Venu's <venubabu.addagada@hcl.com>
2022-01-11 10:12:32 +0530

    exporting to project

---
+++ src/main/java/com/mobile/application/mobileapplication/MobileConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.application.mobileapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile/application/mobileapplication/MobileConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.application.mobileapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
